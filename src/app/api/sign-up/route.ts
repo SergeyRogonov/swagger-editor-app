87226d1bbec40b828bf15e0a72e9d9c0541d7e4d
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-07-01 04:30:35 +0300

    feat: sign-up endpoint added

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,95 @@
+import isValidEmail from "@/utils/isValidEmail/isValidEmail";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    const { email, password } = body;
+
+    if (!email) {
+      return NextResponse.json(
+        { message: "Не указан параметр email" },
+        { status: 400 },
+      );
+    }
+
+    if (!isValidEmail(email)) {
+      return NextResponse.json(
+        { message: "Вы передаете не валидный email" },
+        { status: 400 },
+      );
+    }
+
+    if (!password) {
+      return NextResponse.json(
+        { message: "Не указан параметр password" },
+        { status: 400 },
+      );
+    }
+
+    const cookieStore = await cookies();
+    const supabase = await createClient(cookieStore);
+
+    const { data: usersByEmail, error: errorFindUserByEmail } = await supabase
+      .from("users")
+      .select("id")
+      .eq("email", email)
+      .limit(1);
+
+    if (errorFindUserByEmail) {
+      return NextResponse.json(
+        {
+          message: errorFindUserByEmail.message,
+        },
+        { status: 500 },
+      );
+    }
+
+    if (usersByEmail.length > 0) {
+      return NextResponse.json(
+        {
+          message: "Такой email уже занят",
+        },
+        { status: 409 },
+      );
+    }
+
+    const { data: newUser, error: errorCreateNewUser } = await supabase
+      .from("users")
+      .insert([
+        {
+          email: email,
+          passwordHash: password,
+        },
+      ])
+      .select();
+
+    if (errorCreateNewUser) {
+      return NextResponse.json(
+        {
+          message: errorCreateNewUser.message,
+        },
+        { status: 500 },
+      );
+    }
+
+    console.log(newUser);
+
+    return NextResponse.json(
+      {
+        message: "Вы зарегистрированы",
+      },
+      { status: 201 },
+    );
+  } catch (exception) {
+    return NextResponse.json(
+      {
+        message: `${exception}`,
+      },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/utils/isValidEmail/isValidEmail.ts
@@ -0,0 +1,4 @@
+export default function isValidEmail(email: string): boolean {
+  const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return EMAIL_REGEX.test(email);
+}
