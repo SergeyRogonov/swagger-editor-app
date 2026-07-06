e2083f6e784472aef61edcaf1701c29d986a25d4
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-07-06 23:18:56 +0300

    feat: favicons added

---
+++ src/features/favicon/HeadFavicon.tsx
@@ -0,0 +1,147 @@
+export default function HeadFavicon() {
+  return (
+    <head>
+      <meta
+        property="og:site_name"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="/" />
+      <meta property="og:image" content="/favicon/favicon_256.jpg" />
+      <meta
+        property="og:title"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+      <meta
+        property="og:description"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:image" content="/favicon/favicon_256.jpg" />
+      <meta
+        property="twitter:title"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+      <meta
+        property="twitter:description"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+
+      <link rel="image_src" href="/favicon/favicon_256.jpg" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/favicon/favicon_57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/favicon/favicon_60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/favicon/favicon_72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/favicon/favicon_76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/favicon/favicon_114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/favicon/favicon_120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/favicon/favicon_144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/favicon/favicon_152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/favicon_180.png"
+      />
+
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/favicon/favicon_152.png"
+        sizes="152x152"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/favicon/favicon_120.png"
+        sizes="120x120"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/favicon/favicon_76.png"
+        sizes="76x76"
+      />
+      <link rel="apple-touch-icon-precomposed" href="/favicon/favicon_57.png" />
+
+      <link rel="mask-icon" href="/favicon/favicon.svg" color="#5bbad5" />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon/favicon_192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon/favicon_96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon_32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon_16.png"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link
+        rel="shortcut icon"
+        type="image/vnd.microsoft.icon"
+        href="/favicon/favicon.svg"
+      />
+      <meta
+        name="application-name"
+        content="Swagger UI (Coffee-Driven Development)"
+      />
+      <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+
+      <link rel="manifest" href="/favicon/manifest.webmanifest" />
+      <meta name="theme-color" content="#f8f9fa" />
+
+      <meta name="msapplication-config" content="/favicon/ieconfig.xml" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="msapplication-TileImage" content="/favicon/favicon_144.png" />
+    </head>
+  );
+}
