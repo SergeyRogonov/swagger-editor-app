ce47ac4fe5fec00b5d90fb79984288f69660f447
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-07-08 22:51:00 +0300

    feat: translate added for sign in and sing up

--- src/app/[locale]/sign-in/page.tsx
+++ src/app/[locale]/sign-in/page.tsx
@@ -120,7 +120,7 @@ export default function SignInPage() {
               return (
                 <div>
                   <label htmlFor={ID} className={LABEL_CLASS_NAME}>
-                    E-mail
+                    {t("emailTitle")}
                   </label>
                   <div className="mt-1 pt-1">
                     <input
@@ -145,13 +145,13 @@ export default function SignInPage() {
                 <div>
                   <div className="flex items-center justify-between">
                     <label htmlFor={ID} className={LABEL_CLASS_NAME}>
-                      Пароль
+                      {t("passwordTitle")}
                     </label>
                     <Link
                       href="/forgot-password"
                       className={`text-sm ${LINK_CLASS_NAME}`}
                     >
-                      Забыли пароль?
+                      {t("forgetPassword")}
                     </Link>
                   </div>
                   <div className="relative mt-1 pt-1">
@@ -174,13 +174,13 @@ export default function SignInPage() {
             className="w-full rounded-md bg-blue-600 px-3 py-2 text-sm font-medium hover:bg-blue-500 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:opacity-70"
             disabled={!isValid || isFetch}
           >
-            {isFetch ? "Отправка формы" : "Войти"}
+            {isFetch ? t("sendingForm") : t("signIn")}
           </button>
         </form>
         <p className="mt-6 text-center text-sm text-slate-500 dark:text-slate-400">
-          Нет аккаунта?{" "}
+          {t("noAccount")}{" "}
           <Link href="/sign-up" className={`font-mediumg ${LINK_CLASS_NAME}`}>
-            Зарегистрироваться
+            {t("signUp")}
           </Link>
         </p>
       </div>
--- src/app/[locale]/sign-up/page.tsx
+++ src/app/[locale]/sign-up/page.tsx
@@ -121,7 +121,7 @@ export default function SignUpPage() {
               return (
                 <div>
                   <label htmlFor={ID} className={LABEL_CLASS_NAME}>
-                    E-mail
+                    {t("emailTitle")}
                   </label>
                   <div className="mt-1 pt-1">
                     <input
@@ -146,7 +146,7 @@ export default function SignUpPage() {
                 <div>
                   <div className="flex items-center justify-between">
                     <label htmlFor={ID} className={LABEL_CLASS_NAME}>
-                      Пароль
+                      {t("passwordTitle")}
                     </label>
                   </div>
                   <div className="relative mt-1 pt-1">
@@ -169,13 +169,13 @@ export default function SignUpPage() {
             className="w-full rounded-md bg-blue-600 px-3 py-2 text-sm font-medium hover:bg-blue-500 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:opacity-70"
             disabled={!isValid || isFetch}
           >
-            {isFetch ? "Отправка формы" : "Зарегистрироваться"}
+            {isFetch ? t("sendingForm") : t("signUp")}
           </button>
         </form>
         <p className="mt-6 text-center text-sm text-slate-500 dark:text-slate-400">
-          Есть аккаунт?{" "}
+          {t("isHaveAccount")}{" "}
           <Link href="/sign-in" className={`font-mediumg ${LINK_CLASS_NAME}`}>
-            Войти
+            {t("signIn")}
           </Link>
         </p>
       </div>
