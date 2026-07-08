ad80d2815cca61a21793f3e3268bf2240394a917
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-07-08 23:14:18 +0300

    feat: translate added for header

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -3,21 +3,23 @@
 import { useAuth } from "@/provider/AuthProvider";
 import Link from "next/link";
 import LanguageSwitcher from "./LangSwitcher";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 export function Header() {
   const { isAuth, isLoading } = useAuth();
   const locale = useLocale();
 
+  const t = useTranslations("header");
+
   if (isLoading) {
-    return <div>Проверка авторизации {locale}</div>;
+    return <div>{t("authCheck")}</div>;
   }
 
   return (
     <header className="sticky top-0 z-50 border-b border-slate-800 bg-slate-950/90 backdrop-blur">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <Link href="/" className="text-lg font-semibold">
-          Swagger Editor App
+          {t("appName")}
         </Link>
         <nav className="flex items-center gap-4">
           <LanguageSwitcher />
@@ -25,7 +27,7 @@ export function Header() {
             href={`/${locale}/about`}
             className="text-sm text-slate-300 hover:text-white"
           >
-            About
+            {t("about")}
           </Link>
           {isAuth ? (
             <>
@@ -33,13 +35,13 @@ export function Header() {
                 href={`/${locale}/history`}
                 className="rounded-md border border-slate-700 px-3 py-2 text-sm hover:bg-slate-800"
               >
-                History
+                {t("history")}
               </Link>
               <Link
                 href={`/${locale}/sign-out`}
                 className="rounded-md bg-red-600 px-3 py-2 text-sm font-medium hover:bg-red-500"
               >
-                Sign Out
+                {t("signOut")}
               </Link>
             </>
           ) : (
@@ -48,13 +50,13 @@ export function Header() {
                 href={`/${locale}/sign-in`}
                 className="rounded-md border border-slate-700 px-3 py-2 text-sm hover:bg-slate-800"
               >
-                Sign In
+                {t("signIn")}
               </Link>
               <Link
                 href={`/${locale}/sign-up`}
                 className="rounded-md bg-blue-600 px-3 py-2 text-sm font-medium hover:bg-blue-500"
               >
-                Sign up
+                {t("signUp")}
               </Link>
             </>
           )}
