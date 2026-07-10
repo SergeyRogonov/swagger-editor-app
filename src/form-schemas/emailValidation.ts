15fc8956d8ecb7670fb8472afc1fc3b1c6cdb79f
pavelhalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-07-10 23:04:32 +0300

    feat: translation added for sign forms

--- src/form-schemas/emailValidation.ts
+++ src/form-schemas/emailValidation.ts
@@ -1,96 +1,96 @@
+import { useTranslations } from "next-intl";
 import * as yup from "yup";
 
-const emailValidation = yup
-  .string()
-  .required()
-  .test({
-    message: "one @",
-    test: (value) => {
-      if (!value) {
-        return;
-      }
-
-      return value.split("@").length === 2;
-    },
-  })
-  .test({
-    message: "non-empty local part",
-    test: (value) => {
-      if (!value) {
-        return;
-      }
-
-      const ARRAY = value.split("@");
-      if (ARRAY.length < 2) {
-        return;
-      }
-
-      const LOCAL_PART = ARRAY[0];
-      return LOCAL_PART.length > 0;
-    },
-  })
-  .test({
-    message: "Email must contain domain and site name",
-    test: (value) => {
-      if (!value) {
-        return;
-      }
-
-      const ARRAY = value.split("@");
-      if (ARRAY.length < 2) {
-        return;
-      }
-
-      const DOMAIN_PART = ARRAY[1];
-      const DOMAINS = DOMAIN_PART.split(".");
-
-      for (let i = 0; i < DOMAINS.length; i++) {
-        if (DOMAINS[i].length < 1) {
+export default function emailValidation(t: ReturnType<typeof useTranslations>) {
+  return yup
+    .string()
+    .required(t("email.required"))
+    .test({
+      message: t("email.oneAt"),
+      test: (value) => {
+        if (!value) {
           return;
         }
-      }
 
-      return DOMAINS.length >= 2;
-    },
-  })
-  .test({
-    message:
-      "User name available: A-Z, a-z, 0-9, '.', '_'. User name not start andn end with '.'",
-    test: (value) => {
-      if (!value) {
-        return;
-      }
-
-      const ARRAY = value.split("@");
-      if (ARRAY.length < 2) {
-        return;
-      }
-
-      const USER_NAME_PART = ARRAY[0];
-
-      if (USER_NAME_PART.length == 0) {
-        return;
-      }
-
-      if (USER_NAME_PART[0] === ".") {
-        return;
-      }
-
-      if (USER_NAME_PART[USER_NAME_PART.length - 1] === ".") {
-        return;
-      }
-
-      const AVAILABLE_SYMBOLS_ON_USER_NAME_PART =
-        "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm._1234567890";
-      for (let i = 0; i < USER_NAME_PART.length; i++) {
-        const CHAR = USER_NAME_PART[i];
-        if (!AVAILABLE_SYMBOLS_ON_USER_NAME_PART.includes(CHAR)) {
+        return value.split("@").length === 2;
+      },
+    })
+    .test({
+      message: t("email.localPart"),
+      test: (value) => {
+        if (!value) {
           return;
         }
-      }
 
-      return true;
-    },
-  });
+        const ARRAY = value.split("@");
+        if (ARRAY.length < 2) {
+          return;
+        }
 
-export default emailValidation;
+        const LOCAL_PART = ARRAY[0];
+        return LOCAL_PART.length > 0;
+      },
+    })
+    .test({
+      message: t("email.domain"),
+      test: (value) => {
+        if (!value) {
+          return;
+        }
+
+        const ARRAY = value.split("@");
+        if (ARRAY.length < 2) {
+          return;
+        }
+
+        const DOMAIN_PART = ARRAY[1];
+        const DOMAINS = DOMAIN_PART.split(".");
+
+        for (let i = 0; i < DOMAINS.length; i++) {
+          if (DOMAINS[i].length < 1) {
+            return;
+          }
+        }
+
+        return DOMAINS.length >= 2;
+      },
+    })
+    .test({
+      message: t("email.nameSymbols"),
+      test: (value) => {
+        if (!value) {
+          return;
+        }
+
+        const ARRAY = value.split("@");
+        if (ARRAY.length < 2) {
+          return;
+        }
+
+        const USER_NAME_PART = ARRAY[0];
+
+        if (USER_NAME_PART.length == 0) {
+          return;
+        }
+
+        if (USER_NAME_PART[0] === ".") {
+          return;
+        }
+
+        if (USER_NAME_PART[USER_NAME_PART.length - 1] === ".") {
+          return;
+        }
+
+        const AVAILABLE_SYMBOLS_ON_USER_NAME_PART =
+          "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm._1234567890";
+        for (let i = 0; i < USER_NAME_PART.length; i++) {
+          const CHAR = USER_NAME_PART[i];
+          if (!AVAILABLE_SYMBOLS_ON_USER_NAME_PART.includes(CHAR)) {
+            return;
+          }
+        }
+
+        return true;
+      },
+    });
+}
--- src/form-schemas/useFormSchema.ts
+++ src/form-schemas/useFormSchema.ts
@@ -1,10 +1,11 @@
 import * as yup from "yup";
 import emailValidation from "./emailValidation";
 import passwordValidation from "./passwordValidation";
+import { useTranslations } from "next-intl";
 
-export function getFormSchema() {
+export function getFormSchema(t: ReturnType<typeof useTranslations>) {
   return yup.object({
-    email: emailValidation,
-    password: passwordValidation,
+    email: emailValidation(t),
+    password: passwordValidation(t),
   });
 }
