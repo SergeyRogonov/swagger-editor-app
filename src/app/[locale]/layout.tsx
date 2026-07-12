8f8cf3c828adcad10a41acbc5698aa6ed4cefa1a
Sergey Rogonov <serega.rogonov@gmail.com>
2026-07-13 02:10:15 +0300

    fix: remove double scroller

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -36,10 +36,10 @@ export default async function LocaleLayout({
   return (
     <NextIntlClientProvider messages={messages}>
       <AuthProvider>
-        <div className="flex min-h-screen flex-col">
+        <div className="flex h-screen min-h-0 flex-col">
           <Header />
 
-          <main className="flex-1">
+          <main className="flex-1 min-h-0 overflow-auto">
             <ErrorBoundary>{children}</ErrorBoundary>
           </main>
 
